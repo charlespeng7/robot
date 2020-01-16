8b2b3c3d428f81d221637b043d867f75d35b0a07
wujian <wujian@ones.ai>
2020-01-16 16:05:53 +0800

    update:test.py

--- Test/test.py
+++ Test/test.py
@@ -2,12 +2,9 @@
 #-------------------------------------------------------------------------------
 with True:
     num = 1
-    alist = []
     add = num+1
     if add<100:
-        alist.append(add)
-        for i in range(add):
-            break
+        print(add)
 
 
 
