55a04cd5dc27497939d8bf4444679f66cc47045e
WJ-Q8808 <54425201+WJ-Q8808@users.noreply.github.com>
2020-02-03 15:31:59 +0800

    Update robottest.java

--- robottest.java
+++ robottest.java
@@ -4,7 +4,7 @@ public class Hello {
 @RequestMapping("/")
 public String sayHello(){
 //修改返回的字符串的内容
-return "abcdefg. Hello jenkins, " + new Date();
+return "abcdefg. Hello jenkins , " + new Date();
 }
 }
 
